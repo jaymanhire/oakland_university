cd2eb3f245d2fb092e97017be8e6d25dd4024437
Jay Manhire <jaymanhire@gmail.com>
2018-09-25 14:08:43 -0400

    Homework_1

---
+++ Info/src/Info.java
@@ -0,0 +1,17 @@
+
+public class Info {
+
+	public static void main(String[] args) {
+
+		String name = "Jay Manhire";
+		String favBook = "The Linux Command Line";
+		String favMovie = "Goodfellas";
+
+		System.out.println("My name is " + name + ".");
+		System.out.println("My favorite book: \"" + favBook + "\"");
+		System.out.println("My favorite movie is \"" + favMovie + ".\"");
+		
+
+	}
+
+}
