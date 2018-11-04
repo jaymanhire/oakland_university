b82b6eeec8891bb5a32319957758f0954ccfb786
Jay Manhire <jaymanhire@gmail.com>
2018-11-03 22:06:21 -0400

    Switch Statement

---
+++ Listing_6_1/src/GradeReport.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class GradeReport {
+
+	public static void main(String[] args) {
+
+		int grade;
+		int category;
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter a numeric grade (0 to 100): ");
+		grade = scan.nextInt();
+
+		category = grade / 10;
+
+		System.out.print("That grade is ");
+
+		switch (category) {
+		case 10:
+			System.out.println("a perfect score. Well done!");
+			break;
+		case 9:
+			System.out.println("well above average. Excellent.");
+			break;
+		case 8:
+			System.out.println("above average. Nice job.");
+			break;
+		case 7:
+			System.out.println("average.");
+			break;
+		case 6:
+			System.out.println("below average.");
+			break;
+		default:
+			System.out.println("terrible. You suck, and you failed, loser.");
+
+		}
+		scan.close();
+
+	}
+
+}
