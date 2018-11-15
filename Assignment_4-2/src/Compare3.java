ef90c19fcef630a5ecffc16960035a21f6964124
Jay Manhire <jaymanhire@gmail.com>
2018-11-15 00:46:00 -0500

    Final Deliverable compareTo

--- Assignment_4-2/src/Compare3.java
+++ Assignment_4-2/src/Compare3.java
@@ -1,29 +1,42 @@
 
 public class Compare3 {
-	
-	// determines the largest of three Comparable objects
-	  public static <T extends Comparable<T>> T largest(T x, T y, T z) {
-	    T max = x; 
 
-	    if (y.compareTo(max) > 0)
-	      max = y; 
+	public static void largest(String str1, String str2, String str3) {
+		
+		 int var1 = str1.compareTo( str2 );
+		 int var2 = str1.compareTo( str3 );
+		 int var3 = str2.compareTo( str1 );
+		 int var4 = str2.compareTo( str3 );
+		 int var5 = str3.compareTo( str1 );
+		 int var6 = str3.compareTo( str2 );
 
-	    if (z.compareTo(max) > 0)
-	      max = z; 
+		if (var1 > 0 && var2 > 0)
+			System.out.println("\"" + str1 + "\" is largest");
+		/*System.out.println(var1);
+		System.out.println(var2);*/
 
-	    return max; 
-	  } 
+		if (var3 > 0 && var4 > 0)
+			System.out.println("\"" + str2 + "\" is largest");
+		/*System.out.println(var3);
+		System.out.println(var4);*/
 
-/*	public static void largest(String str1, String str2, String str3) {
+		if (var5 > 0 && var6 > 0)
+			System.out.println("\"" + str3 + "\" is largest");
+		/*System.out.println(var5);
+		System.out.println(var6);*/
 
-		if (str1.compareTo(str2) < 0 && str1.compareTo(str3) < 0) {
-			System.out.println("str1 is less than str2 and str3");
-		}
-		if (str2.compareTo(str3) < 0) {
-			System.out.println("str2 is less than str3");
-		}
-		if (str3) {
+		
+		// Or, as Below
+	/*	if (str1.compareTo(str2) > 0 && str1.compareTo(str3) > 0)
+			System.out.println("\"" + str1 + "\" is largest");
 
-		}
+		else if (str2.compareTo(str1) > 0 && str2.compareTo(str3) > 0)
+			System.out.println("\"" + str2 + "\" is largest");
+
+		else
+			System.out.println("\"" + str3 + "\" is largest");
+
 	}*/
+
+	}
 }
