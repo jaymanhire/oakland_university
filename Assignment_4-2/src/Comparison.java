be6c620d5cb443c6ef443be2f131b017efe05ead
Jay Manhire <jaymanhire@gmail.com>
2018-11-16 12:47:53 -0500

    Final Assignment_4-2rev

--- Assignment_4-2/src/Comparison.java
+++ Assignment_4-2/src/Comparison.java
@@ -17,6 +17,20 @@ public class Comparison {
 
 		Compare3.largest(str1, str2, str3);
 		
+		System.out.println("----------------------------");
+		
+		String int1, int2, int3;		
+
+		System.out.println("Enter integer 1 of 3: ");
+		int1 = scan.nextLine();
+		System.out.println("Enter integer 2 of 3: ");
+		int2 = scan.nextLine();
+		System.out.println("Enter integer 3 of 3: ");
+		int3 = scan.nextLine();
+
+		Compare3.largest(int1, int2, int3);
+		
+		
 		
 		scan.close();
 
