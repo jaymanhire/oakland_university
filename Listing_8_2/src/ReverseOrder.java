783dd8aea28c43e751c83dbd43301343edaf3297
Jay Manhire <jaymanhire@gmail.com>
2018-11-19 19:07:23 -0500

    Basic Array reversed

---
+++ Listing_8_2/src/ReverseOrder.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ReverseOrder {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		double[] numbers = new double[10];
+
+		System.out.println("The size of the array: " + numbers.length);
+
+		for (int index = 0; index < numbers.length; index++) {
+			System.out.println("Enter number " + (index + 1) + ": ");
+			numbers[index] = scan.nextDouble();
+
+		}
+		System.out.println("The numbers in reverse order: ");
+
+		for (int index = numbers.length - 1; index >= 0; index--) {
+			System.out.println("Number " + (index + 1) + ": " + numbers[index] + " ");
+		}
+		scan.close();
+	}
+
+}
