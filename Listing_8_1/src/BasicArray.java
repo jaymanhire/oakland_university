a66262b24c983a4aeb927bfe631b87c51cae06bb
Jay Manhire <jaymanhire@gmail.com>
2018-11-19 19:06:27 -0500

    Basic Array

---
+++ Listing_8_1/src/BasicArray.java
@@ -0,0 +1,29 @@
+
+public class BasicArray {
+
+	public static void main(String[] args) {
+
+		final int LIMIT = 15;
+		final int MULTIPLE = 10;
+
+		int[] list = new int[LIMIT];
+
+		// Initialize the array values
+		for (int index = 0; index < LIMIT; index++) {
+			list[index] = index * MULTIPLE;
+		}
+
+		list[5] = 999;
+		
+		for(int value: list) {
+			System.out.print(value + "  ");
+		}
+
+		/*for (int i = 0; i < list.length; i++) {
+			System.out.print(list[i]);
+			System.out.print("\t");
+
+		} */
+	}
+
+}
