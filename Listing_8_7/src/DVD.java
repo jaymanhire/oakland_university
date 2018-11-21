85a41c307eef7b0dbe23dbd8a215aca06d6a6f4c
Jay Manhire <jaymanhire@gmail.com>
2018-11-21 02:32:21 -0500

    Use of an array of objects.

---
+++ Listing_8_7/src/DVD.java
@@ -0,0 +1,35 @@
+
+// Represents a DVD video disc
+
+import java.text.NumberFormat;
+
+public class DVD {
+	private String title, director;
+	private int year;
+	private double cost;
+	private boolean bluray;
+
+	// Creates a new DVD with the specified information
+
+	public DVD(String title, String director, int year, double cost, boolean bluray) {
+		this.title = title;
+		this.director = director;
+		this.year = year;
+		this.cost = cost;
+		this.bluray = bluray;
+	}
+
+	// Returns a string description of this DVD
+
+	public String toString() {
+		NumberFormat fmt = NumberFormat.getCurrencyInstance();
+		String description;
+
+		description = fmt.format(cost) + "\t" + year + "\t";
+		description += title + "\t" + director;
+		if (bluray)
+			description += "\t" + "Blu-ray";
+		return description;
+	}
+
+}
---
+++ Listing_8_7/src/Movies.java
@@ -0,0 +1,26 @@
+
+public class Movies {
+
+	// Creates DVD collection object and adds some DVD's to it. Prints
+	// reports on the status of the collection.
+
+	public static void main(String[] args) {
+
+		DVDCollection movies = new DVDCollection();
+
+		movies.addDVD("The Godfather", "Francis Ford Coppola", 1972, 24.95, true);
+		movies.addDVD("The Godfather", "Francis Ford Coppola", 1972, 24.95, true);
+		movies.addDVD("The Godfather", "Francis Ford Coppola", 1972, 24.95, true);
+		movies.addDVD("The Godfather", "Francis Ford Coppola", 1972, 24.95, true);
+		movies.addDVD("The Godfather", "Francis Ford Coppola", 1972, 24.95, true);
+
+		System.out.println(movies);
+
+		movies.addDVD("The Godfather", "Francis Ford Coppola", 1972, 24.95, true);
+		movies.addDVD("The Godfather", "Francis Ford Coppola", 1972, 24.95, true);
+
+		System.out.println(movies);
+
+	}
+
+}
