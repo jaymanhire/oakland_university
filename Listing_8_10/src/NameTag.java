68b769242cf67a90d53f68b9fe40d031173163af
Jay Manhire <jaymanhire@gmail.com>
2018-11-21 15:47:21 -0500

    Command line arguments

---
+++ Listing_8_10/src/NameTag.java
@@ -0,0 +1,14 @@
+
+public class NameTag {
+
+	public static void main(String[] args) {
+
+		// In the terminal, supply the 2 args right after "java"(run command):
+
+		System.out.println();
+		System.out.println("    " + args[0]);
+		System.out.println("My name is: " + args[1]);
+
+	}
+
+}
